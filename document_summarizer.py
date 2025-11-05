359d0f44480387653b472e9da99264851403ae42
91964 <yeswanthsoma83@gmail.com>
2025-11-04 19:51:40 -0800

    Add comments to code

--- document_summarizer.py
+++ document_summarizer.py
@@ -9,6 +9,7 @@ def extract_text_from_pdf(file_path):
         with open(file_path, "rb") as file:
             reader = PyPDF2.PdfReader(file)
 # Added comment
+# Added comment
             text = ""
             for page in reader.pages:
                 text += page.extract_text()
